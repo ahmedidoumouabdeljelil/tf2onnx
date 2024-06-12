4cd5162f23e7d61fbfb34aafa4b79712d1c745cd
ahmedidoumouabdeljelil <155551223+ahmedidoumouabdeljelil@users.noreply.github.com>
2024-06-12 16:21:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET')
+app.config['databaseURL]= os.environ.get('')
 socketio = SocketIO(app)
 
 firebaseConfig = {
